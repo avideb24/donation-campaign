ee7286d31a165c47908d22c67dcef7ce45b48bcd
avideb24 <debavi24@gmail.com>
2023-09-25 02:50:42 +0600

    update ErrorPage

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center gap-4 w-full h-[100vh]">
 
-            <h2>Oops!!!</h2>
-            <p>Something went wrong!!!</p>
+            <h2 className="text-5xl text-red-600 font-bold">Oops!!!</h2>
+            <p className="text-2xl text-black font-bold">Something went wrong!!!</p>
+            <Link to='/'><button className="btn btn-error text-white">Go Home</button></Link>
             
         </div>
     );
