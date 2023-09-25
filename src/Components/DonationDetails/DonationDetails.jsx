58bc7b76ecd5716f846b42d953e93416e914fc40
avideb24 <debavi24@gmail.com>
2023-09-25 11:50:49 +0600

    DonationPage added and complete localstorage job

--- src/Components/DonationDetails/DonationDetails.jsx
+++ src/Components/DonationDetails/DonationDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast';
 
 const DonationDetails = () => {
 
@@ -9,12 +10,41 @@ const DonationDetails = () => {
 
     const donation = donations.find(donation => donation.id === id);
 
+    const handleDonate = () => {
+        const donationsArr = [];
+
+        const addedDonation = JSON.parse(localStorage.getItem('donations'));
+        if (!addedDonation) {
+            donationsArr.push(donation)
+            localStorage.setItem('donations', JSON.stringify(donationsArr))
+            toast.success('You Have Donated Successfully.');
+            return
+        }
+        else {
+            const existDonation = addedDonation.find(item => item.id === donation.id);
+
+            if (existDonation) {
+                toast.error("You Can't Donate Twice.");
+                return 
+            }
+            else {
+                donationsArr.push(...addedDonation, donation)
+                localStorage.setItem('donations', JSON.stringify(donationsArr))
+                toast.success('You Have Donated Successfully.');
+                return
+            }
+
+        }
+    }
+
+
     return (
         <div className="max-w-7xl mx-auto pb-16">
             <div className="relative">
                 <img className="w-full h-[500px] object-cover rounded-lg" src={donation.img} alt="img" />
                 <div className="absolute z-10 w-full h-24 left-0 bottom-0 bg-black opacity-60 rounded-b-lg"></div>
-                <button className="btn btn-error absolute left-7 bottom-6 z-20 text-white px-6">${donation.price}</button>
+                <button onClick={handleDonate} className="absolute left-7 bottom-6 z-20 text-white px-4 py-3 rounded-lg" style={{backgroundColor: `${donation.text_color}`}}>Donate ${donation.price}</button>
+                <Toaster></Toaster>
             </div>
             <h2 className="text-black text-3xl font-bold my-6 pt-4">{donation.title}</h2>
             <p className="text-justify">{donation.description}</p>
--- src/Components/DonationPage/DonationPage.jsx
+++ src/Components/DonationPage/DonationPage.jsx
@@ -1,8 +1,78 @@
+import { useEffect, useState } from "react";
 
 const DonationPage = () => {
+
+    const [donations, setDonations] = useState([]);
+    const [noData, setNoData] = useState();
+    const [isShow, setIsShow] = useState(false)
+
+    useEffect(() => {
+        const storedDonation = JSON.parse(localStorage.getItem('donations'))
+
+        if (storedDonation) {
+            setDonations(storedDonation)
+        }
+        else {
+            setNoData('No Data Found')
+        }
+
+    }, [])
+
+
     return (
-        <div>
-            <p>Donations</p>
+        <div className="max-w-7xl mx-auto py-10">
+            <div>
+                {
+                    donations.length === 0 ? <div className="flex justify-center items-center w-full h-[100vh]">{noData}</div> :
+                        <div>
+                            {
+                                isShow ?
+                                    <div className="grid grid-cols-2 gap-4">
+                                        {
+                                            donations.map(donation =>
+                                                <div key={donation.id} className="flex">
+                                                    <img className="rounded-bl-lg rounded-tl-lg rounded-tr-none" src={donation.img} alt="" />
+                                                    <div className='p-5 rounded-tr-lg rounded-br-lg' style={{ backgroundColor: `${donation.card_bg}`, color: `${donation.text_color}` }}>
+                                                        <p className='inline-block p-1 rounded px-2 text-sm' style={{ backgroundColor: `${donation.category_bg}` }}>{donation.category}</p>
+                                                        <h2 className='text-xl font-semibold mt-2'>{donation.title}</h2>
+                                                        <p>${donation.price}</p>
+                                                        <button className="text-white px-4 py-3 rounded-lg mt-4" style={{ backgroundColor: `${donation.text_color}` }}>View Details</button>
+                                                    </div>
+                                                </div>)
+                                        }
+                                    </div> :
+                                    <div className="grid grid-cols-2 gap-4">
+                                        {
+                                            donations.slice(0, 2).map(donation =>
+                                                <div key={donation.id} className="flex">
+                                                    <img className="rounded-bl-lg rounded-tl-lg rounded-tr-none" src={donation.img} alt="" />
+                                                    <div className='p-5 rounded-tr-lg rounded-br-lg' style={{ backgroundColor: `${donation.card_bg}`, color: `${donation.text_color}` }}>
+                                                        <p className='inline-block p-1 rounded px-2 text-sm' style={{ backgroundColor: `${donation.category_bg}` }}>{donation.category}</p>
+                                                        <h2 className='text-xl font-semibold mt-2'>{donation.title}</h2>
+                                                        <p>${donation.price}</p>
+                                                        <button className="text-white px-4 py-3 rounded-lg mt-4" style={{ backgroundColor: `${donation.text_color}` }}>View Details</button>
+                                                    </div>
+                                                </div>)
+                                        }
+                                    </div>
+                            }
+                        </div>
+                }
+            </div>
+            <div className="text-center">
+                {
+                    donations.length > 2 &&
+                    <div>
+                        {
+                            isShow === false ?
+                                <div className="text-center my-4 ">
+                                    <button onClick={() => setIsShow(!isShow)} className="btn btn-success text-white">See More</button>
+                                </div> : 
+                                ''
+                        }
+                    </div>
+                }
+            </div>
         </div>
     );
 };
